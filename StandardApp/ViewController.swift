d3ccb7f71850a0d40108df37dadc32fcaa6583c1
Rone Loza <rone.loza@mambo.pe>
2019-05-07 10:52:40 -0500

    Initial Commit

---
+++ StandardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StandardApp
+//
+//  Created by Rone Loza on 5/7/19.
+//  Copyright © 2019 Developer Rone Loza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
